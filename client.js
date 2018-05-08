7bb5da8af1ccfcf1645d433c217000f73377bedb
Marta Jopp <martapeterson@gmail.com>
2018-05-07 21:53:34 -0500

    getting data

--- client.js
+++ client.js
@@ -3,3 +3,17 @@ console.log('sourced');
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
 const cities = [];
+
+//fetch returns the promise/what comes back
+//blob needs to be converted from raw data to json
+fetch(endpoint)
+.then(blob => blob.json())
+//...spreads the array so pushes each separately es6
+.then(data => cities.push(...data))
+
+function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+//here we need to figure out if it matches what was searched
+
+    })
+}
