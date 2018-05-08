17d48b5a3f85b16a1e0f44a8b32fc4267f9a12c9
Marta Jopp <martapeterson@gmail.com>
2018-05-07 22:16:51 -0500

    number with commas

--- client.js
+++ client.js
@@ -20,6 +20,10 @@ function findMatches(wordToMatch, cities) {
     })
 }
 
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 function displayMatches() {
 const matchArray = findMatches(this.value, cities)
 const html = matchArray.map(place => {
@@ -30,7 +34,7 @@ const html = matchArray.map(place => {
     return `
     <li>
     <span class="name">${cityName}, ${stateName}</span>
-    <span class="population">${place.population}</span>
+    <span class="population">${numberWithCommas(place.population)}</span>
     </li>
     `;
 }).join('');
